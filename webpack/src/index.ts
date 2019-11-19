a88eff8b59bef43dd1b3c1b93eb5950d4f98a662
Benjamin Segall <bentekkie@gmail.com>
2019-11-19 18:47:25 -0500

    planetsssssss

--- webpack/src/index.ts
+++ webpack/src/index.ts
@@ -3,55 +3,73 @@ import './style.css'
 // three.js
 import * as THREE from 'three'
 import { Planet } from './planet'
-import { Vector3 } from 'three'
+import { Vector3, MeshPhongMaterial, MeshBasicMaterial, MeshStandardMaterial } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
+import * as dat from 'dat.gui';
+ 
+const gui = new dat.GUI();
 // create the scene
 let scene = new THREE.Scene()
-
 
 const textureSources = [
 	{
 		source:"textures/mercury.jpg",
-		orbitalVelocity: .01
+		orbitalVelocity: 1.607
 	}, {
 		source:"textures/venus.jpg",
-		orbitalVelocity: .02
+		orbitalVelocity: 1.174
 	}, {
 		source:"textures/earth.jpg",
-		orbitalVelocity: .03
+		orbitalVelocity: 1
 	}, {
 		source:"textures/mars.jpg",
-		orbitalVelocity: .02
+		orbitalVelocity: 	0.802
 	}, {
 		source:"textures/jupiter.jpg",
-		orbitalVelocity: .001
+		orbitalVelocity: 0.434
 	}, {
 		source:"textures/saturn.jpg",
-		orbitalVelocity: .025
+		orbitalVelocity: 0.323
 	}, {
 		source:"textures/venus.jpg",
-		orbitalVelocity: .002
+		orbitalVelocity: 0.228
 	}, {
 		source:"textures/venus.jpg",
-		orbitalVelocity: .07
+		orbitalVelocity: 0.182
 	}
 ]
 
+
+const sunRadius = 1;
 const planets = textureSources.map((src,i) => {
-		const texture = new THREE.TextureLoader().load( src.source );
-		texture.wrapS = THREE.RepeatWrapping;
-		texture.wrapT = THREE.RepeatWrapping;
-		texture.repeat.set( 1, 1 );
-		return new Planet((i+1)*1,.3,src.orbitalVelocity,0.01,new Vector3(0,0,0), texture, true);});
+		const map = new THREE.TextureLoader().load( src.source );
+		map.wrapS = THREE.RepeatWrapping;
+		map.wrapT = THREE.RepeatWrapping;
+		map.repeat.set( 1, 1 );
+		return new Planet(sunRadius + (i+1)*1.5,.1*(i+1), 0,0.01*src.orbitalVelocity,0.01,new Vector3(0,0,0), new MeshStandardMaterial({map}));});
 
 
+const moon = new Planet(.5,.1,0,.001,.001,new Vector3(0,0,0),planets[2].material)
+planets[2].addMoon(moon)
+
+for (let i = 0; i < 100; i++) {
+	const moon = new Planet(1.1,.05,.01,.01*(i+1),.001,new Vector3(0,0,0),planets[5].material)
+	planets[5].addMoon(moon)
+	
+}
+
+for (let i = 0; i < 100; i++) {
+	const moon = new Planet(.9,.05,.01, .01*(i+1),.001,new Vector3(0,0,0),planets[5].material)
+	planets[5].addMoon(moon)
+	
+}
 
 // create the camera
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-let renderer = new THREE.WebGLRenderer()
-
+const renderer = new THREE.WebGLRenderer()
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 // set size
 renderer.setSize(window.innerWidth, window.innerHeight)
 
@@ -64,16 +82,45 @@ let axis = new THREE.AxesHelper(10)
 scene.add(axis)
 
 const sunLight = new THREE.PointLight( 0xffffff, 5, 50 );
-
-const sun = new Planet(0,1,.01,.01,new Vector3(0,0,0),new THREE.TextureLoader().load( "textures/sun.jpg" ),false)
+sunLight.castShadow = true;
+sunLight.shadow.mapSize.width = 1024; // default
+sunLight.shadow.mapSize.height = 1024; // default
+sunLight.shadow.camera.near = 1; // default
+sunLight.shadow.camera.far = 1000 // default
+sunLight.shadow.radius = 10
+const sun = new Planet(0,sunRadius,0,.001,.001,new Vector3(0,0,0),new MeshBasicMaterial({map:new THREE.TextureLoader().load( "textures/sun.jpg" )}))
 sunLight.add(sun)
 scene.add(sunLight)
+const ambient = new THREE.AmbientLight( 0x404040 ); // soft white light
 
+scene.add( ambient );
+
+var helper = new THREE.CameraHelper( sunLight.shadow.camera );
+scene.add( helper );
 planets.push(sun)
 
 //scene.add(box)
 
 planets.forEach(planet => scene.add(planet))
+//This will add a starfield to the background of a scene
+var starsGeometry = new THREE.Geometry();
+
+for ( var i = 0; i < 10000; i ++ ) {
+
+	var star = new THREE.Vector3();
+	star.x = THREE.Math.randFloatSpread( 2000 );
+	star.y = THREE.Math.randFloatSpread( 2000 );
+	star.z = THREE.Math.randFloatSpread( 2000 );
+
+	starsGeometry.vertices.push( star );
+
+}
+
+var starsMaterial = new THREE.PointsMaterial( { color: 0x888888 } );
+
+var starField = new THREE.Points( starsGeometry, starsMaterial );
+
+scene.add( starField );
 
 
 camera.position.x = 5
@@ -95,6 +142,5 @@ function render(): void {
 	planets.forEach(p => p.tick())
 	renderer.render(scene, camera)
 }
-
 
 animate()
--- webpack/src/planet.ts
+++ webpack/src/planet.ts
@@ -1,35 +1,41 @@
 
 import {Sphere,Vector3, Mesh, SphereGeometry, MeshBasicMaterial, Object3D, Material, Texture, MeshPhongMaterial} from 'three'
 export class Planet  extends Object3D{
-
     private mesh : Mesh
-    private pivot : Object3D
+    private moons : Planet[] = []
     constructor(
         private orbitRadius : number,
         private planetRadius: number,
+        private apogee: number,
         private orbitalVelocity: number,
         private spinVelocity: number,
         private pivotPoint: Vector3,
-        texture: Texture,
-        phong: boolean){
+        public material: Material){
             super();
-            if(phong){
-                this.mesh = new Mesh(new SphereGeometry( planetRadius, 32, 32 ),new MeshPhongMaterial({map:texture}))
-            } else {
-                this.mesh = new Mesh(new SphereGeometry( planetRadius, 32, 32 ),new MeshBasicMaterial({map:texture}))
-                
-            }
+            this.mesh = new Mesh(new SphereGeometry( planetRadius, 32, 32 ),material)
             this.translateX(this.pivotPoint.x);
             this.translateY(this.pivotPoint.y);
             this.translateZ(this.pivotPoint.z);
             this.mesh.translateX(orbitRadius);
+            this.mesh.castShadow = true;
+            this.mesh.receiveShadow = true;
+            this.castShadow = true;
+            this.receiveShadow = true;
+            this.rotateX(apogee)
             this.add(this.mesh)
+    }
+
+
+    public addMoon(moon : Planet) {
+        this.mesh.add(moon)
+        this.moons.push(moon)
     }
 
 
     public tick() {
         this.rotation.y += this.orbitalVelocity
         this.mesh.rotation.y += this.spinVelocity
+        this.moons.forEach(m => m.tick())
     }
 }
 
